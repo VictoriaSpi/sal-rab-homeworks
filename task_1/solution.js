982fd261d2b76913964d4a6154103efe6f19a40f
VictoriaSpi <amigo445@gmail.com>
2022-10-16 18:38:22 +0300

    task 1 done done done

--- task_1/solution.js
+++ task_1/solution.js
@@ -11,7 +11,7 @@ function addInCartMessage(title, price) {
     message = message + " "; // добавьте в конец переменной message пробел
     message = message + productPrice; // добавьте в конец переменной message значение переменной productPrice
     message = message + " "; // добавьте в конец переменной message пробел
-    message = message + "теперь в корзине"; // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + "теперь в корзине!"; // добавьте в конец переменной message текст 'теперь в корзине!'
     console.log(message); // выведите переменную message в консоль
     // Конец решения задания №1.1.
 
